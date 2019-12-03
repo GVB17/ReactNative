432d0b63cfd8d3493d3bd4d83bf34ad1b2eb8427
Venkatesh <guttiivenkatesh@gmail.com>
2019-12-03 23:39:28 +0530

    React Native Sample App

--- RNApp/app/screens/splash/index.js
+++
@@ -1,3 +0,0 @@
-import Splash from './components/splash';
-
-export { Splash };
--- RNApp/index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './app/samples/ReduxSample2/reduxSample2App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
